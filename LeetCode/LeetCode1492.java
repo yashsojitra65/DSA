ce4040512e0bb5bb61044e49d1c7f61916975e09
Yash Sojitra <yashsojitra065@gmail.com>
2025-11-17 18:12:09 +0530

    17-11-2025 Second commit

--- LeetCode/LeetCode1492.java
+++ LeetCode/LeetCode1492.java
@@ -1,2 +1,25 @@
-package LeetCode;public class LeetCode1492 {
+package LeetCode;
+
+import java.util.*;
+
+//17-11-2025
+//1492. The kth Factor of n.
+public class LeetCode1492 {
+    public static void main(String[] args) {
+        int n = 12;
+        int k = 3;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+
+        if (list.size() < k) {
+            System.out.println(-1);
+        } else {
+            System.out.println(list.get(k - 1));
+        }
+    }
 }
